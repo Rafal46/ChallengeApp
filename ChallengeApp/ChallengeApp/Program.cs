6aeaff48644b97517692a64606e6d1ad3981dbc7
Rafal77 <raf-pio@wp.pl>
2023-02-14 19:52:17 +0100

    Zadanie lekcja 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string name = "Ania";
+string sex = "Kobieta";
+int age = 20;
+
+
+Console.WriteLine(name);
+Console.WriteLine(sex);
+Console.WriteLine(age);
+
+if (sex == "Kobieta")
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+        if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+
+    else
+    {
+        Console.WriteLine("Kobieta 30 lat lub starsza");
+    }
+
+else
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+
+    else
+    {
+        Console.WriteLine("Pełnoletni mężczyzna");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
